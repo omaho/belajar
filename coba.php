d102d51a2ee57e4298585046150e795f50b70a06
omaho <shuderoger@gmail.com>
2021-05-27 15:46:30 +0700

    mater

---
+++ coba.php
@@ -0,0 +1,23 @@
+<?php
+
+//echo "Hello world\n";
+/*
+$apple = "di jual di pasar";
+$appleKedua = 200.40;
+
+echo $apple."\n";
+echo $appleKedua;
+*/
+
+// pengenalan kondisi
+$umur = 18;
+if ($umur == 20){
+    echo "umur saya ".$umur;
+}else if($umur == 18){
+    echo "umur saya ".$umur;
+}
+else{
+    echo "Kue wis tuo";
+}
+
+?>
